f6abf6204e112a4daa7cc74ca4410b48cb9f8257
Joe Barnes <joe@joescii.com>
2017-09-26 11:40:07 -0500

    First pass at making Jetty9Starter configurable

--- jetty9/src/main/scala/net/liftmodules/cluster/jetty9/Jetty9Config.scala
+++ jetty9/src/main/scala/net/liftmodules/cluster/jetty9/Jetty9Config.scala
@@ -1,5 +1,96 @@
 package net.liftmodules.cluster.jetty9
 
-class Jetty9Config {
+import java.net.URI
 
+import net.liftweb.common.{Box, Failure, Full}
+
+/**
+  * Config object for jetty 9
+  * @param port port number in which to run the app
+  * @param contextPath the root context path to make your application available at
+  * @param clusterConfig optional configuration for clustering
+  */
+case class Jetty9Config (
+  port: Int,
+  contextPath: String,
+  clusterConfig: Option[Jetty9ClusterConfig]
+)
+
+/**
+  * Config object for jetty 9 clustering
+  * @param workerName name for the node/worker, must be unique within cluster!
+  * @param jdbcDriver jdbc driver for the cluster's backend store
+  * @param sqlEndpointConfig jdbc endpoint configuration
+  */
+case class Jetty9ClusterConfig (
+  workerName: String,
+  jdbcDriver: SqlDriver,
+  sqlEndpointConfig: SqlEndpointConfig
+)
+
+/**
+  * Config object for SQL endpoints
+  */
+trait SqlEndpointConfig {
+  /**
+    * Full URL endpoint string including the 'jdbc:' prefix
+    */
+  def endpoint: String
+}
+
+object SqlEndpointConfig {
+
+  /**
+    * Fully-specified SQL endpoint
+    * @param vendor the SQL vendor such as MySQL, Postgres, etc
+    * @param host the SQL server hostname
+    * @param port the SQL server port
+    * @param dbName the database to use for jetty clustering tables
+    * @param user the SQL db username
+    * @param password the SQL db password
+    */
+  def apply(vendor: SqlVendor, host: String, port: Int, dbName: String, user: String, password: String): SqlEndpointConfig = new SqlEndpointConfig() {
+    override def endpoint: String = s"jdbc:$vendor://$host:$port/$dbName?user=$user&password=$password"
+  }
+
+  /**
+    * Digs around in the system ENV to give you the correct config while running in Heroku.
+    * Currently only their MySQL equivalent ClearDB is supported.
+    */
+  def forHeroku: Box[SqlEndpointConfig] = if(System.getenv("CLEARDB_DATABASE_URL") == null) {
+    Failure("Could not find environment variable CLEARDB_DATABASE_URL")
+  } else {
+    val dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"))
+    val username = dbUri.getUserInfo.split(":")(0)
+    val password = dbUri.getUserInfo.split(":")(1)
+
+    Full(new SqlEndpointConfig {
+      override def endpoint: String = s"jdbc:$VendorMysql://${dbUri.getHost}${dbUri.getPath}?user=$username&password=$password&${dbUri.getQuery}"
+    })
+  }
+}
+
+/**
+  * A SQL Vendor such as MySQL, etc
+  */
+sealed trait SqlVendor
+case object VendorMysql extends SqlVendor {
+  override def toString: String = "mysql"
+}
+case class VendorOther(name: String) extends SqlVendor {
+  override def toString: String = name
+}
+
+/**
+  * Jdbc SQL driver
+  */
+sealed trait SqlDriver
+case object DriverMysql extends SqlDriver {
+  override def toString: String = "com.mysql.jdbc.Driver"
+}
+case object DriverMariaDB extends SqlDriver {
+  override def toString: String = "org.mariadb.jdbc.Driver"
+}
+case class DriverOther(className: String) extends SqlDriver {
+  override def toString: String = className
 }
--- jetty9/src/main/scala/net/liftmodules/cluster/jetty9/Jetty9Starter.scala
+++ jetty9/src/main/scala/net/liftmodules/cluster/jetty9/Jetty9Starter.scala
@@ -1,25 +1,16 @@
 package net.liftmodules.cluster.jetty9
 
-import java.net.URI
-
 import net.liftweb.common.Loggable
-import net.liftweb.util.{LoggingAutoConfigurer, Props, StringHelpers}
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.session.{JDBCSessionIdManager, JDBCSessionManager}
 import org.eclipse.jetty.webapp.WebAppContext
 
-import scala.util.Properties
-
+/**
+  * Object for starting a Lift app with Jetty 9
+  */
 object Jetty9Starter extends Loggable {
   def start(config: Jetty9Config): Unit = {
-    LoggingAutoConfigurer().apply()
-
     logger.info("starting Lift server...")
-
-    val port = System.getProperty(
-      "jetty.port", Properties.envOrElse("PORT", "8080")).toInt
-
-    logger.debug(s"port number is $port")
 
     val webappDir: String = Option(this.getClass.getClassLoader.getResource("webapp"))
       .map(_.toExternalForm)
@@ -28,32 +19,17 @@ object Jetty9Starter extends Loggable {
 
     logger.debug(s"webappDir: $webappDir")
 
-    val server = new Server(port)
-    val context = new WebAppContext(webappDir, Props.get("jetty.contextPath").openOr("/"))
+    val server = new Server(config.port)
+    val context = new WebAppContext(webappDir, config.contextPath)
 
-    if(Props.get("cluster").map(_.equalsIgnoreCase("true")).openOr(false)) {
-      val workerName = StringHelpers.randomString(10)
+    config.clusterConfig.foreach { clusterConfig =>
+      logger.info(s"Worker/Node name: ${clusterConfig.workerName}")
 
-      logger.info(s"Worker/Node name: $workerName")
-
-      val driver = Props.get("cluster.jdbc.driver").openOrThrowException("Cannot boot in cluster mode without property 'session.jdbc.driver' defined in props file")
-
-      val endpoint = if (System.getenv("CLEARDB_DATABASE_URL") == null) {
-        // Non-heroku deployment. Either local or AWS
-        val dbHost = Properties.envOrElse("DB_HOST", "127.0.0.1")
-        val dbPort = Properties.envOrElse("DB_PORT", "3306")
-        s"jdbc:mysql://$dbHost:$dbPort/lift_sessions?user=jetty&password=lift-rocks"
-      } else {
-        // Heroku deployment
-        val dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"))
-        val username = dbUri.getUserInfo.split(":")(0)
-        val password = dbUri.getUserInfo.split(":")(1)
-        s"jdbc:mysql://${dbUri.getHost}${dbUri.getPath}?user=$username&password=$password&${dbUri.getQuery}"
-      }
+      val endpoint = clusterConfig.sqlEndpointConfig.endpoint
 
       val idMgr = new JDBCSessionIdManager(server)
-      idMgr.setWorkerName(workerName)
-      idMgr.setDriverInfo(driver, endpoint)
+      idMgr.setWorkerName(clusterConfig.workerName)
+      idMgr.setDriverInfo(clusterConfig.jdbcDriver.toString, endpoint)
       idMgr.setScavengeInterval(60)
       idMgr.setBlobType("LONGBLOB")
       server.setSessionIdManager(idMgr)
@@ -65,7 +41,7 @@ object Jetty9Starter extends Loggable {
 
     server.setHandler(context)
     server.start()
-    logger.info(s"Lift server started on port $port")
+    logger.info(s"Lift server started on port ${config.port}")
     server.join()
   }
 
